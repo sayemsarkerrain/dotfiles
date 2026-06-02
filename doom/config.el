3ab53d3da7422bfcae5c238f12eafcc2053d932d
sayemsarkerink <sayemsarker.me@gmail.com>
2026-06-03 00:50:03 +0600

    backed up

---
+++ doom/config.el
@@ -0,0 +1,75 @@
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type 'relative)
+
+(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one-light)
+
+;;(setq doom-font (font-spec :family "Fira Code" :size 13))
+(setq doom-font (font-spec :family "Hack" :size 13))
+
+(setq org-agenda-files
+      (directory-files-recursively "~/org/" "\\.org$"))
+
+(setq org-agenda-skip-scheduled-if-done t
+      org-agenda-skip-deadline-if-done t)
+
+(map! :leader
+      :desc "org-agenda" "a" #'org-agenda)
+
+(setq org-agenda-sorting-strategy
+      '((tags tag-up)))
+
+(map! :leader
+      :desc "org-capture" "c" #'org-capture)
+
+(after! org
+  (setq org-capture-templates
+        '(
+          ("t" "tasks" entry
+           (file "~/org/tasks.org")
+           "* TODO %U %? :todos:\n")
+
+          ("i" "ideas" entry
+           (file "~/org/ideas.org")
+           "* IDEA %U %? :ideas:\n")
+
+          ("j" "journal")
+          ("jr" "reflections" entry
+           (file+olp+datetree "~/org/journal.org")
+           "* %U Reflection %? :reflections:\n")
+          ("jo" "observations" entry
+           (file+olp+datetree "~/org/journal.org")
+           "* %U %? :observations:\n"))))
+
+(setq org-todo-keywords
+      '((sequence
+         "TODO(t)"
+         "PROJ(p)"
+         "LOOP(l)"
+         "STRT(s)"
+         "WAIT(w)"
+         "HOLD(h)"
+         "IDEA(i)"
+         "NOTE(n)"
+         "|"
+         "DONE(d)"
+         "KILL(k)"
+         "ARCH(a)"
+         "PROC(P)")))
+
+(setq evil-escape-key-sequence "jk")
+(setq evil-escape-delay 0.2)
+
+(setq ispell-program-name "hunspell")
+
+(setq ispell-hunspell-dict-paths-alist
+      '(("en_US" "C:/Hunspell/dict/en_US.aff")))
+
+(setq ispell-local-dictionary "en_US")
+
+(setq delete-by-moving-to-trash t)
+
+(setq confirm-kill-emacs nil)
+
+(setq initial-buffer-choice "~/org/projects/")
